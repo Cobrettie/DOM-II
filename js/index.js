fc66a3d0935d5f64d3933a56c896e495eb1e1c7e
Cobrettie <cobrettiegarner3@gmail.com>
2020-02-13 20:17:27 -0500

    add event listeners for each nav-link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// adding event listener to nav items
+let navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(item => {
+  item.addEventListener('mouseover', event => {
+    event.target.style.fontSize = '2rem';
+    event.target.style.transition = '.3s';
+    event.target.style.borderBottom = '2px solid dodgerblue';
+  })
+  item.addEventListener('mouseleave', event => {
+    event.target.style.fontSize = '1.6rem';
+    event.target.style.borderBottom = 'none';
+  })
+});
+
