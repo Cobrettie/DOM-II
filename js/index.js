1dd460f87d14f4215e441393e9313ce3c8b23d2b
Cobrettie <cobrettiegarner3@gmail.com>
2020-02-13 20:25:22 -0500

    add event listeners to destination buttons at bottom of page

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@
 let navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach(item => {
   item.addEventListener('mouseover', event => {
-    event.target.style.fontSize = '2rem';
     event.target.style.transition = '.3s';
+    event.target.style.fontSize = '2rem';
     event.target.style.borderBottom = '2px solid dodgerblue';
   })
   item.addEventListener('mouseleave', event => {
@@ -13,4 +13,16 @@ navLinks.forEach(item => {
     event.target.style.borderBottom = 'none';
   })
 });
+
+// add event listener to destination buttons at bottom of page
+let destBtn = document.querySelectorAll('.btn');
+destBtn.forEach(item => {
+  item.addEventListener('mouseover', event => {
+    event.target.style.transition = '.3s';
+    event.target.style.fontSize = '2.2rem';
+  })
+  item.addEventListener('mouseleave', event => {
+    event.target.style.fontSize = '1.8rem';
+  })
+})
 
